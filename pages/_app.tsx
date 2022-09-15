310da5e04b6c00f9e7544f6a1d5a888a2a79034d
Angoose <angoose.dev@gmail.com>
2022-09-15 15:56:04 -0500

    Configure TailwindCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+	<div className="bg-zinc-800 text-white min-h-screen">
+		<Component {...pageProps} />
+	</div>
+  );
 }
 
 export default MyApp
