b0f73cde069e7d972d54de130d7a126ef59cbef0
Angoose <angoose.dev@gmail.com>
2022-09-16 14:24:12 -0500

    Add Login or Create Account switcher to auth page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,5 @@
 import { Button, Input } from '@components/common';
+import { motion } from 'framer-motion';
 import { NextPage } from 'next';
 import { signIn } from 'next-auth/react';
 import { FormEvent, useState } from 'react';
@@ -6,6 +7,8 @@ import { FormEvent, useState } from 'react';
 const Auth: NextPage = () => {
 	const [email, setEmail] = useState<string>('');
 	const [password, setPassword] = useState<string>('');
+
+	const [authType, setAuthType] = useState<'login'|'create'>('login');
 
 	const handleSignIn = (e: FormEvent) => {
 		e.preventDefault();
@@ -25,14 +28,35 @@ const Auth: NextPage = () => {
 		<div className="p-6 bg-zinc-900 flex flex-col max-w-sm m-auto translate-y-1/3 rounded-md shadow-md">
 			<form onSubmit={handleSignIn} className="flex flex-col">
 				<h3 className="font-bold text-2xl text-center mb-3">todo-app</h3>
+				<div className="flex flex-row items-center text-center mb-2 cursor-pointer bg-zinc-800 rounded-md shadow-md relative select-none">
+					<motion.div
+						className={`absolute w-1/2 h-full bg-violet-500 rounded-md ${authType === 'login' ? 'left-0' : 'right-0'}`}
+						transition={{
+							type: 'spring',
+							stiffness: 700,
+							damping: 40
+						}}
+						layout
+					/>
+					<div className={`grow basis-0 z-10 p-2 transition-colors ${authType !== 'login' ? 'text-zinc-400' : ''}`} onClick={() => setAuthType('login')}>Login</div>
+					<div className={`grow basis-0 z-10 p-2 transition-colors ${authType !== 'create' ? 'text-zinc-400' : ''}`}  onClick={() => setAuthType('create')}>Create Account</div>
+				</div>
 				<Input placeholder="Email" className="my-1" onChange={(e) => setEmail(e.target.value)}/>
 				<Input placeholder="Password" className="my-1" type="password" onChange={(e) => setPassword(e.target.value)}/>
 				<Button className="mt-3" type="submit" disabled={!email || !password || email === '' || password === ''}>
-					Login
+					<motion.div
+						key={authType}
+						initial={{ y: 10, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						exit={{ y: -10, opacity: 0 }}
+						transition={{ duration: 0.2 }}
+					>
+						{authType === 'login' ? 'Login' : 'Create Account'}
+					</motion.div>
 				</Button>
 			</form>
 		</div>
-	)
+	);
 }
 
 export default Auth;
