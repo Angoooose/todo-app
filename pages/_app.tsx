3852bd19aa2d402e9d10e025a4cf6706ff51a3d2
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-09-20 21:05:55 -0500

    Add Head tags (title & favicon)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,16 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
+import Head from 'next/head';
 
 function App({ Component, pageProps: { session, ...pageProps} }: AppProps<{ session: Session }>) {
   return (
 	<SessionProvider session={session}>
+		<Head>
+			<link rel="icon" href="/favicon.ico" sizes="any" />
+    		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+			<title>todo-app</title>
+		</Head>
 		<div className="bg-zinc-800 text-white min-h-screen">
 			<Component {...pageProps} />
 		</div>
